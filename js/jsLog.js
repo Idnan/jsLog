dc3e74756718cd14d68a5da8d1cd9eab18904878
adnan ahmed <mahrradnan@gmail.com>
2014-11-09 13:08:54 +0500

    added custom options functionality

--- js/jsLog.js
+++ js/jsLog.js
@@ -1,46 +1,77 @@
-var jsLog = function() {
-
-	takeOverConsole = function() {
-		console.log = interceptLog;
-	}
-
-	interceptLog = function(arguments) {
-		if(arguments) {
-			var log = parseLog(arguments);
-			appendToViewer(log);
-		}
-	}
-
-	appendToViewer = function(log) {
-		$("<p>"+log+"</p>").hide().appendTo('.jslogviewer').fadeIn('slow');
-		scrollToBottom();
-	}
-
-	scrollToBottom = function() {
-		$('.jslogviewer').scrollTop($('.jslogviewer').prop('scrollHeight'));
-	}
-
-	parseLog = function(log) {
-		return JSON.stringify(log);
-	}
-
-	initViewer = function() {
-		var viewer = "<div class='jslogviewer'><span id='minimize'>-</span></div>";
-		$(viewer).appendTo('body');
-	}
-
-	return {
-
-		init:  function() {
-			this.bindUI();
-			initViewer();
-		},
-		bindUI: function() {
-			takeOverConsole();
-			interceptLog();
-		}
-	}
+;
+// jslog plugin created by Adnan Ahmed
+// v0.1
+if ( typeof Object.create !== 'function' ) {
+    Object.create = function( obj ) {
+        function F() {}
+        F.prototype = obj;
+        return new F();
+    };
 }
+(function( $, window, document, undefined ) {
+    "use strict";
 
-var jsLog = new jsLog();
-jsLog.init();
+    var JSLog = {
+
+        init: function (options, elem) {
+            
+            this.options = $.extend( {}, $.jsLog.options, options );           
+
+            // Start the processing
+            this.bindUI();
+            this.initViewer();
+        },
+
+        bindUI: function () {
+
+            this.takeOverConsole();
+            this.interceptLog();
+        },
+
+        takeOverConsole : function() {
+            console.log = this.interceptLog;
+        },
+
+        interceptLog : function(log) {
+            if(log) {
+                JSLog.appendToViewer(JSON.stringify(log));
+            }
+        },
+
+        appendToViewer : function(log) {
+            this.el = "<p>"+log+"</p>";            
+            $(this.el).hide().appendTo('.jslogviewer').fadeIn('slow');
+            this.scrollToBottom();
+        },
+
+        scrollToBottom : function() {
+            $('.jslogviewer').scrollTop($('.jslogviewer').prop('scrollHeight'));
+        },        
+
+        initViewer : function() {
+            this.viewerEl = $("<div class='jslogviewer'></div>");
+            this.viewerEl.css({
+                'width': this.options.width,
+                'height': this.options.height,
+                'background': this.options.background,
+                'top': this.options.top,
+                'right': this.options.right
+            });
+            $(this.viewerEl).appendTo('body');
+        }
+    };
+
+    $.jsLog = function(options) {
+        var jsLog = Object.create( JSLog );
+        jsLog.init(options, this);
+    };
+
+    $.jsLog.options = {
+        width: '490px',
+        height: '200px',
+        background: 'rgba(0, 0, 0, 0.5)',
+        top: '5px',
+        right: '5px'
+    };
+
+})( jQuery, window, document );
---
+++ js/jslog2.js
@@ -0,0 +1,46 @@
+var jsLog = function() {
+
+    takeOverConsole = function() {
+        console.log = interceptLog;
+    }
+
+    interceptLog = function(arguments) {
+        if(arguments) {
+            var log = parseLog(arguments);
+            appendToViewer(log);
+        }
+    }
+
+    appendToViewer = function(log) {
+        $("<p>"+log+"</p>").hide().appendTo('.jslogviewer').fadeIn('slow');
+        scrollToBottom();
+    }
+
+    scrollToBottom = function() {
+        $('.jslogviewer').scrollTop($('.jslogviewer').prop('scrollHeight'));
+    }
+
+    parseLog = function(log) {
+        return JSON.stringify(log);
+    }
+
+    initViewer = function() {
+        var viewer = "<div class='jslogviewer'></div>";
+        $(viewer).appendTo('body');
+    }
+
+    return {
+
+        init:  function() {
+            this.bindUI();
+            initViewer();
+        },
+        bindUI: function() {
+            takeOverConsole();
+            interceptLog();
+        }
+    }
+}
+
+var jsLog = new jsLog();
+jsLog.init();
