886edba3df9cfd029027d2b8f9966666145756e2
adnan ahmed <mahrradnan@gmail.com>
2014-12-10 23:39:24 +0500

    made some modifications

--- jquery.jslog.js
+++ jquery.jslog.js
@@ -1,6 +1,10 @@
 ;
-// jslog plugin created by Adnan Ahmed
-// v0.1
+/*! Copyright (c) 2014 Adnan Ahmed (http://adnanahmed.info)
+ * and GPL (http://www.opensource.org/licenses/gpl-license.php) licenses.
+ *
+ * Version: 1.0.0
+ *
+ */
 if ( typeof Object.create !== 'function' ) {
     Object.create = function( obj ) {
         function F() {}
@@ -14,16 +18,18 @@ if ( typeof Object.create !== 'function' ) {
     var JSLog = {
 
         init: function () {
-
-            // Generate the custom console
             JSLog.generateConsole();
-            // Take over the browser console
             JSLog.takeOverConsole();
         },
 
         takeOverConsole : function() {
-            console.original = console.log;
-            window.onerror = JSLog.interceptError;
+
+            this.wrapper = document.querySelector(".console-scroll-container");
+            this.div = document.querySelector(".console-rows");
+
+            window.onerror = JSLog.interceptError;            
+            if (!console) window.console = {};
+            this._log = console.log;
             console.log = JSLog.interceptLog;
         },
 
@@ -33,21 +39,86 @@ if ( typeof Object.create !== 'function' ) {
             }
         },
 
-        interceptLog : function(log) {
-            if(log) {
-                JSLog.appendToViewer(JSON.stringify(log));
-                console.original(log);
+        interceptLog : function() {
+            var args = arguments;
+            this._log && this._log.apply(console, args);
+
+            var log = '';
+            if (typeof args[0] === "string" && args.length > 1 && /((^|[^%])%[sdifoO])/.test(args[0])) {
+                log = format.apply(null, args);
+            } else {
+                log = [].map.call(args, JSLog.getString).join(" ");
+            }            
+            JSLog.appendToViewer(log, '');
+        },
+
+        isInstance : function(val, typeName) {
+            return val instanceof window[typeName];
+        },
+
+        getString : function(val, wrapString) {
+            if (typeof val === "string") {
+                return wrapString ? '"' + val + '"' : val;
+            } else if (val == null || typeof val === "number" || typeof val === "function" || this.isInstance(val, "Date")) {
+                return "" + val;
+            } else if (this.isInstance(val, "HTMLElement")) {
+                return val.outerHTML;
+            } else {
+                try {
+                    return JSON.stringify(val, function (k, v) {
+                        if (this.isInstance(v, "HTMLElement")) {
+                            return v.outerHTML;
+                        } else if (typeof v === "function") {
+                            return "" + v;
+                        }
+                        return v;
+                    });
+                } catch (err) {
+                    return Object.prototype.toString.call(val);
+                }
             }
         },
 
-        appendToViewer : function(log, type) {
-            JSLog.logEl = $("<p></p>", {
-               text : log,
-               class : ( type === 'error' ) ? 'clog-error' : 'clog-msg',
-            });
+        format: function() {
+            var i = 0,
+                val,
+                args = arguments;
 
-            JSLog.consoleViewerEl.append( JSLog.logEl );
-            $.proxy( JSLog.scrollToBottom, this);
+            return args[0].replace(/(%?%[sdifoO])/g, function (c) {
+
+                if (c.length === 3) return c;
+
+                val = args[++i];
+
+                if (val == null) {
+                    return "" + val;
+                }
+
+                switch (c.charAt(1)) {
+                    case "s":
+                        return val;
+                    case "d":
+                    case "i":
+                        return typeof val === "number" ? Math.floor(val) : "NaN";
+                    case "f":
+                        return typeof val === "number" ? val : "NaN";
+                    default:
+                        return this.getString(val, true);
+                }
+            });
+        },
+
+        appendToViewer : function(log, type) {
+            
+            var row = document.createElement("div");
+            row.className = "console-row";
+
+            var code = row.appendChild(document.createElement("code"));
+            code.className = ( type === 'error' ) ? 'console-row-code console-row-code-error' : 'console-row-code';
+            code.textContent = log;
+
+            JSLog.div.appendChild(row);
+            JSLog.wrapper.scrollTop = row.offsetTop;
         },
 
         scrollToBottom : function() {
@@ -55,11 +126,7 @@ if ( typeof Object.create !== 'function' ) {
         },
 
         generateConsole : function() {
-
-            JSLog.consoleViewerEl = $("<div></div>", {
-                class : "clog-viewer"
-            });
-
+            JSLog.consoleViewerEl = '<div class="console"> <div class="console-title">Snippet Console</div> <div class="console-scroll-container"> <div class="console-rows"></div> </div> </div>';
             $('body').append( JSLog.consoleViewerEl );
         },
 
